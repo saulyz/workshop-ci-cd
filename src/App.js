839427be2a3890a3b2259890d146bcf36d27f1b4
Saulius Vikerta <svikerta@kayak.com>
2019-05-29 18:24:07 +0300

    adding state to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './dice.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('initial message');
 
   const handleClick = () => {
     console.log('was click');
+    setMessage('Button clicked');
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>message</p>
+        <p>{message}</p>
         <button className="button" onClick={handleClick}>Button</button>
       </header>
     </div>
