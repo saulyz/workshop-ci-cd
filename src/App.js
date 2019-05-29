cef0e0caf43c36726ac6cb8b11665b92d7eefd09
Saulius Vikerta <svikerta@kayak.com>
2019-05-29 18:58:32 +0300

    connecting frontend with backend

--- src/App.js
+++ src/App.js
@@ -3,18 +3,37 @@ import logo from './dice.svg';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('initial message');
+  const [message, setMessage] = useState(
+    'No queries have initiated'
+  );
+
+  const [status, setStatus] = useState(
+    'default'
+  );
 
   const handleClick = () => {
-    console.log('was click');
-    setMessage('Button clicked');
+    setStatus('processing');
+    fetch('/api')
+      .then(response => response.json() )
+      .then(response => {
+        console.log(response);
+        setMessage(response.message);
+        if (response.status) {
+          setStatus('success');
+        } else {
+          setStatus('failure');
+        }
+      })
+      .catch(() => {
+        setMessage('API is not responding. Please try next time.');
+      });
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
+        <p className={`message ${status}`}>{message}</p>
         <button className="button" onClick={handleClick}>Button</button>
       </header>
     </div>
